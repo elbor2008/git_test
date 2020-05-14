de09ff642a47119a500d37c8c44ed63b086d8317
Nick Nie <sky_elbor@hotmail.com>
2020-05-14 17:53:50 +1000

    Update BloggingContext.cs
    set name length to 120 from blog table

--- CodeFirst/Models/BloggingContext.cs
+++ CodeFirst/Models/BloggingContext.cs
@@ -36,7 +36,7 @@ namespace CodeFirst.Models
             //{
 
             //});
-            modelBuilder.Entity<Blog>().Property(b => b.Name).HasMaxLength(20);
+            modelBuilder.Entity<Blog>().Property(b => b.Name).HasMaxLength(120);
         }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
